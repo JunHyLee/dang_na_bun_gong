e493d77a92248fb64b7b4a25e9c8bd08f50ab173
JunHyLee <121077745+JunHyLee@users.noreply.github.com>
2023-05-23 23:33:00 +0900

    내질문목록, 자주하는질문목록 완료

--- src/main/java/com/example/dang_na_bun_gong/Repository/CustomerRepository.java
+++ src/main/java/com/example/dang_na_bun_gong/Repository/CustomerRepository.java
@@ -16,16 +16,16 @@ public interface CustomerRepository extends JpaRepository<NotionEntity, Integer>
     //공지사항 목록
     Page<NotionEntity> findAll(Pageable pageable);
 //자주 하는 질문 목록
-    @Query(value = "SELECT quest_title, quest_content_fp, quest_category_id from quest_commen", countQuery = "SELECT COUNT(*) from quest_commen", nativeQuery = true)
+    @Query(value = "SELECT quest_title, quest_content_fp from quest_commen", countQuery = "SELECT COUNT(*) from quest_commen", nativeQuery = true)
     Page<QuestCommenDto> questCommenListAll(Pageable pageable);
 
-    @Query(value = "SELECT quest_title, quest_content_fp, quest_category_id from quest_commen where quest_category_id = :quest_category_id", countQuery = "SELECT COUNT(*) from quest_commen where quest_category_id = :quest_category_id", nativeQuery = true)
+    @Query(value = "SELECT quest_title, quest_content_fp from quest_commen where quest_category_id = :quest_category_id", countQuery = "SELECT COUNT(*) from quest_commen where quest_category_id = :quest_category_id", nativeQuery = true)
     Page<QuestCommenDto> questCommenList(Pageable pageable, Integer quest_category_id);
 
 //내 질문 목록
     @Query(value = "select quest_title, quest_content_fp, quest_answer_fp from quest_private where questioner_id = :questioner_id and quest_category_id = :selected_quest_category_id ", countQuery = "SELECT COUNT(*) from quest_private where questioner_id = :questioner_id and quest_category_id = :selected_quest_category_id", nativeQuery = true)
     Page<QuestPrivateDto> questPrivateList(Pageable pageable, String questioner_id, Integer selected_quest_category_id);
 
-    @Query(value = "select quest_title, quest_content_fp, quest_answer_fp from quest_private where questioner_id = :questioner_id", countQuery = "SELECT COUNT(*) from quest_private where questioner_id = :questioner_id", nativeQuery = true)
+    @Query(value = "select quest_title, quest_content_fp, answer_content_fp from quest_private where questioner_id = :questioner_id", countQuery = "SELECT COUNT(*) from quest_private where questioner_id = :questioner_id", nativeQuery = true)
     Page<QuestPrivateDto> questPrivateListAll(Pageable pageable, String questioner_id);
 }
